9721178e0ab3daad12dc0b33e4a2d28fcc68e3a6
JereHendz <jereomarr@gmal.com>
2022-04-26 21:14:47 -0600

    Change the the domain

--- src/roles.jsx
+++ src/roles.jsx
@@ -1,10 +1,10 @@
-import { useState, React, useEffect } from "react";
+import { React, useEffect } from "react";
 import axios from "axios";
 
 export default function Roles() {
   useEffect(() => {
     axios
-      .get("https://zowis-system.herokuapp.com/api/roles")
+      .get("http://ec2-18-117-137-48.us-east-2.compute.amazonaws.com/zowis-system-new/api/roles")
       .then((payload) => {
         console.log(payload);
         // setNotes(payload.data);
