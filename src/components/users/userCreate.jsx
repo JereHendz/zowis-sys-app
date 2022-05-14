d4ae7e094533902519c57c58acdd4aa6f9d76c0f
ElmerG <elmguz74@hotmail.com>
2022-05-13 22:09:32 -0600

    Add validations to create form

--- src/components/users/userCreate.jsx
+++ src/components/users/userCreate.jsx
@@ -1,19 +1,34 @@
 import React, { Fragment, useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form'
+import { Col, Button, Form, Label, Input, FormGroup, Row, Container, Card, CardHeader, CardBody, CardFooter } from 'reactstrap'
+import { UserCreate, Submit,Cancel,Password, Email, Empleados } from '../../constant'
 import Breadcrumb from '../../layout/breadcrumb'
-import {Container,Row,Col,Card,CardHeader,CardBody,CardFooter,Form,FormGroup,Label,Input,Button} from 'reactstrap'
-import { UserCreate,EmailAddress,Password,Submit,Cancel, Email } from "../../constant";
 import axios from 'axios';
 import { classes } from '../../data/layouts';
 import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
 
-const CreateUser = () => { 
-  
+const TooltipForm = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [validateClass, setValidateClass] = useState(false);
+  const onSubmit = (e, data) => {
+    e.preventDefault();
+    if (data !== '') {
+      alert('You submitted the form and stuff!');
+    } else {
+      errors.showMessages();
+    }
+  };
+
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
+  const [idEmployee, setIdEmployee] = useState("");
   const [password, setPassword] = useState("");
   const [passConfirm, setPassConfirm] = useState("");
+  const [employees, setEmployees] = useState([]);
 
   // Define const that allow see and hide the password
   const [togglePassword, setTogglePassword]=useState(false);
@@ -23,6 +38,7 @@ const CreateUser = () => {
     {
       'userName':'',
       'email':'',
+      'idEmployee':'',
       'password':'',
       'passConfirm':''
     }
@@ -31,10 +47,26 @@ const CreateUser = () => {
   const defaultLayoutObj = classes.find(item => Object.values(item).pop(1) === 'compact-wrapper');
   const layout = localStorage.getItem('layout') || Object.keys(defaultLayoutObj).pop();
 
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_DOMAIN_SERVER}api/employees`)
+    .then((res)=>{
+      //navigate(`${process.env.PUBLIC_URL}/app/users/userEdit/${layout}`);
+      setEmployees(res.data);
+      setEmail(res.data[0].email);
+      toast.success(res.data.message);
+    })
+    .catch((errors)=>{
+      console.log(errors);
+      setLoading(false);
+      setError(errors.response.data.messages);
+    });
+  }, []); 
+
+
   const createUser = (e) => {
     setLoading(true);
-    const data = { userName: userName, email: email, password:password, passConfirm:passConfirm };
-      axios.post(`${process.env.REACT_APP_DOMAIN_SERVER}api/users`, data)
+    const data = { userName: userName, email: email, password:password, passConfirm:passConfirm, idEmployee:idEmployee };
+    axios.post(`${process.env.REACT_APP_DOMAIN_SERVER}api/users`, data)
     .then((payload)=>{
       navigate(`${process.env.PUBLIC_URL}/app/users/userEdit/${layout}`);
       console.log(payload);
@@ -58,43 +90,62 @@ const CreateUser = () => {
                 <span>{"Aquí puedes crear tus usuarios."}</span>
               </CardHeader>
               <CardBody>
-                <Form className="theme-form">
-                <FormGroup>
-                    <Label className="col-form-label pt-0" >{"User Name"}</Label>
-                    <Input className="form-control" type="text" placeholder="Enter the user name" required="" onChange={e => setUserName(e.target.value)} defaultValue={""} />
-                    {/* <small className="form-text text-muted">{"We'll never share your user name with anyone else."}</small> */}
-                    <span className="help is-danger">{error.userName}</span>
-                  </FormGroup>
-                  <FormGroup>
-                    <Label className="col-form-label pt-0" >{Email}</Label>
-                    <Input className="form-control" type="email" placeholder="Enter email" required="" onChange={e => setEmail(e.target.value)} defaultValue={""} />
-                    {/* <small className="form-text text-muted">{"We'll never share your email with anyone else."}</small> */}
-                    <span className="help is-danger">{error.email}</span>
-                  </FormGroup>
-                  <FormGroup>
-                  <div className="position-relative">
-                    <Label htmlFor="exampleInputPassword1">{Password}</Label>
-                    <Input className="form-control" type={togglePassword ? "text" : "password"} placeholder="Password" onChange={e => setPassword(e.target.value)} defaultValue={""} />
-                    <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
-                    <span className="help is-danger">{error.password}</span>
-                
-                  </div>
-                  </FormGroup>
-                  <FormGroup>
-                  <div className="position-relative">
-
-                    <Label htmlFor="exampleInputPassword1">Confirmar contraseña</Label>
-                    <Input className="form-control" type={togglePassword ? "text" : "password"} placeholder="Password" onChange={e => setPassConfirm(e.target.value)} defaultValue={""} />
-                    <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
-                    <span className="help is-danger">{error.passConfirm}</span>
+                <Form className={`needs-validation tooltip-validation ${validateClass ? 'validateClass' : ''}`} noValidate="" onSubmit={handleSubmit(onSubmit)}>
+                  <Row>
+                    <FormGroup>
+                      <Label className="col-form-label pt-0" >{"User Name"}</Label>
+                      <input className="form-control" type="text" placeholder="Enter the user name" name="userName" {...register('userName', { required: true })} onChange={e => setUserName(e.target.value)} defaultValue={""} />
+                      <span>{errors.userName && 'Debe ingresar un nombre de usuario'}</span>
+                      <div className="valid-feedback">{"Looks good!"}</div>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label className="col-form-label pt-0" >{Email}</Label>
+                      <input className="form-control" type="email" placeholder="Enter email" name="email"  {...register('email', { required: true })}/>
+                      <span>{errors.email && 'Debe ingrear un email'}</span>
+                      <div className="valid-feedback">{"Looks good!"}</div>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label className="col-form-label pt-0" >{Empleados}</Label>
+                      <select className="form-control" name="email"  {...register('email', { required: true })} onChange={e => setIdEmployee(e.target.value)} defaultValue={""}>
+                      <option value={""}>{"Seleccione una opción"}</option>
+                        {
+                          employees.length > 0 ?
+                            employees.map((employee) => {
+                              return (
+                                <option value={employee.id}>{employee.firstName+" "+employee.lastName}</option>
+                              );
+                            })
+                          : <option value={""}>{"No hay data"}</option>
+                        }
+                      </select>
+                      <span>{errors.idEmployee && 'Debe seleccionar un empleado'}</span>
+                      <div className="valid-feedback">{"Looks good!"}</div>
+                    </FormGroup>
+                    <FormGroup>
+                    <div className="position-relative">
+                      <Label htmlFor="exampleInputPassword1">{Password}</Label>
+                      <input className="form-control" type={togglePassword ? "text" : "password"} placeholder="Password" name="password" {...register('password', { required: true })} />
+                      <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
+                      <span>{errors.email && 'Debe ingresar una contraseña'}</span>
+                      <div className="valid-feedback">{"Looks good!"}</div>
                     </div>
-                  </FormGroup>
+                    </FormGroup>
+                    <FormGroup>
+                      <div className="position-relative">
+                        <Label htmlFor="exampleInputPassword1">Confirmar contraseña</Label>
+                        <input className="form-control" type={togglePassword ? "text" : "password"} placeholder="Password" name="confirmarPassword"  {...register('confirmarPassword', { required: true })} />
+                        <div className="show-hide" onClick={() => setTogglePassword(!togglePassword)}><span className={togglePassword ? "" : "show"}></span></div>
+                        <span>{errors.confirmarPassword && 'Debe confirmar la contraseña'}</span>
+                        <div className="valid-feedback">{"Looks good!"}</div>
+                      </div>
+                    </FormGroup>
+                  </Row>
+                  <CardFooter>
+                    <Button className="me-1" disabled={loading ? loading : loading} color="primary" type="submit" onClick={() => setValidateClass(!validateClass)}>{loading ? "CREANDO..." : Submit}</Button>
+                    <Button color="secondary">{Cancel}</Button>
+                  </CardFooter>
                 </Form>
               </CardBody>
-              <CardFooter>
-                <Button color="primary" className="me-1" disabled={loading ? loading : loading} onClick={() => createUser(userName, email, password)}>{loading ? "CREANDO..." : Submit}</Button>
-                <Button color="secondary">{Cancel}</Button>
-              </CardFooter>
             </Card>
           </Col>
         </Row>
@@ -103,4 +154,4 @@ const CreateUser = () => {
   );
 };
 
-export default CreateUser;
+export default TooltipForm;
